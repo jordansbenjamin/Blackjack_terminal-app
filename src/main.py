10c071300e430c94c4dc1ff6668978c9c6b23380
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-29 16:27:00 +1000

    Added initial to do's for the main py file

---
+++ src/main.py
@@ -0,0 +1,7 @@
+##### MAIN SECTION #####
+
+# TODO 1: Create a function that will serve as the main function body of the program that will include other parts, sections and functions of the program, this main function will serve as the main flow running the program
+
+# TODO 2: Initialise the deck
+
+# TODO 3: Create a while loop that will prompt the user if they want to continue playing, if yes then call the main function to start the game
