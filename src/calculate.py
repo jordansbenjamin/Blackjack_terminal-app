0d46b416155ed9dd5d47ffb97bb54f5dc51981b3
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-29 17:05:35 +1000

    Added to dos in calculate section

---
+++ src/calculate.py
@@ -0,0 +1,9 @@
+#### CALCULATE SCORES ####
+
+# TODO 1: Create a function that calculates the scores of the cards from a hand, the parameter will be the hand itself, start a score from 0, 
+
+# TODO 2: Remove suits and put it into a cards list for calculation, 
+
+# TODO 3: Convert any picture cards to a score of 10 and Ace to 11 using the cards dictionary
+
+# TODO 4: If Ace card/s are found and score is over 21, count Ace as 1 instead of 11
