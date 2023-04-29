6956c54359302a29cfcd3edfcc11d55707111b86
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-30 08:55:59 +1000

    Added to dos to winner section

---
+++ src/winner.py
@@ -0,0 +1,13 @@
+##### WINNER SECTION #####
+""" Logic that involves determining conditions of winner and displaying the results """
+# Testing docstrings
+
+# TODO 1: Create a function that determines the winner based on their respective conditions
+    # TODO 2: Sum up final player and dealer score
+    # TODO 3: Display both players and dealers final hand and their final score
+
+    # TODO 4: Determine condition of winner/loser based on Natural Blackjack, which means that they have a 21 from the first cards dealt, return their outcome
+
+    # TODO 4: Determine condition of winner/loser based on normal Blackjack, which means that they have a 21 from cards drawn throughout the game, return their outcome
+
+    # TODO 5: Determine all other possible winning/losing conditions, return their outcome
