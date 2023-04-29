d6c88cd2358d4d6c68d47566693474d7914e5611
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-29 21:16:38 +1000

    Added BJ ASCII art logo into arts file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 ##### MAIN SECTION #####
 from clear import clear
+from art import logo
 from player import player_, player_cards
 from dealer import dealer_, dealer_cards
 from cards import init_deck, deal_card
@@ -7,6 +8,7 @@ from cards import init_deck, deal_card
 # TODO 1: Create a function that will serve as the main function body of the program that will include other parts, sections and functions of the program, this main function will serve as the main flow running the program
 def play_blackjack():
     # TODO LATE: Add game logo here
+    print(logo)
 
     deal_card(2, player_cards)
     deal_card(2, dealer_cards)
