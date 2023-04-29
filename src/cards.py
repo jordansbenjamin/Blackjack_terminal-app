061349314d867bdaf98c68057d8fcfa78b79eb01
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-29 16:38:26 +1000

    Added to dos for the cards section of the program

---
+++ src/cards.py
@@ -0,0 +1,11 @@
+#### CARDS SECTION ####
+
+# TODO 1: Create dictionary of cards
+
+# TODO 2: Create list of suits
+
+# TODO 3: Create empty deck list
+
+# TODO 4: Create a function that initialises deck of cards that adds each card to its own respective suits which adds up to a total of 52 deck of cards, additionally shuffle the deck of cards
+
+# TODO 5: Create a function that draws a card from the deck, check condition if deck of cards is empty then initialise the deck, otherwise pop the card from the deck and return that card
