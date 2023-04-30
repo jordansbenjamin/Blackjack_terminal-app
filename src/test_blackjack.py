bbf9287d4b928df29f8858587e44efa0528890b7
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-30 17:13:55 +1000

    Tested calculate score function and passed the test as expected

--- src/test_blackjack.py
+++ src/test_blackjack.py
@@ -16,8 +16,17 @@ def test_calculate_score():
     # Result: Pass ✅
 
     # TEST CASE 2: Test dealer with a hand of four cards and test the sum of their hand
-    # dealer_hand = ['♣6', '♥7']
+    dealer_hand = ['♣6', '♥7']
     # Expected score
-    # dealer_score = 13
+    dealer_score = 13
     # Expected Result: Pass
-    # assert calculate_score(dealer_hand) == dealer_score
+    assert calculate_score(dealer_hand) == dealer_score
+    # Result: ✅
+
+    # TEST CASE 3: Test player with a hand of six cards and test the sum of their hand
+    player_hand = ['♣4', '♥2', '♦4', '♠8', '♦2', '♠8']
+    # Expected score
+    player_score = 28
+    # Expected Result: Pass
+    assert calculate_score(dealer_hand) == dealer_score
+    # Result: ✅
