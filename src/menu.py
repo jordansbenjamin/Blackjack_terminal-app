d6d572decdd3bd7d591b9d690c41ec88a79ce458
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-30 11:29:13 +1000

    Fixed output lines in main menu function and finished initial to dos in main py file

--- src/menu.py
+++ src/menu.py
@@ -2,9 +2,9 @@
 
 # TODO 1: Create menu function displaying all the list of menu items to select from
 def main_menu():
-    print("1. Start playing Blackjack")
-    print("2. Check your score history")
-    print("3. Exit")
+    print("1. Enter 1 to start playing Blackjack")
+    print("2. Enter 2 to check your score history")
+    print("3. Enter 3 to exit Blackjack")
     # TODO 2: Prompt user for their selection and return selection
-    choice = input("Choose a menu option")
+    choice = input("Choose an option from the menu: ")
     return choice
