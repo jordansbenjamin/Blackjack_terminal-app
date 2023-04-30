445deb84d5b508d2f75ccac716e12d8854374bc6
jordansbenjamin <14327@coderacademy.edu.au>
2023-04-30 13:25:29 +1000

    Created initial instructions function

--- src/menu.py
+++ src/menu.py
@@ -1,4 +1,5 @@
 ##### MENU SECTION #####
+from clear import clear_screen
 
 # TODO 1: Create menu function displaying all the list of menu items to select from
 def main_menu():
@@ -12,5 +13,24 @@ def main_menu():
     return choice
 
 # TODO 3: Create a function that stores information of the games instructions
+def instructions():
+    print("""
+    The premise of the game is simple, get as close to 21 as possible without going over.
+    As the player you will be dealt 2 cards, the dealer will also have 2 cards.
+    However, the dealer will only present one of its card.
+    As the player, you can decide to 'hit' for another card or 'pass' to skip a turn.
+    If you get a Natural Blackjack (An Ace and a picture card) you instantly win the game.
+    If the dealer gets a Natural Blackjack, you instantly lose the game.
+    The game will end if either the players or dealers final hand score is over 21.
+    The game will end if either player or dealers final hand score is less than each others.
+    """)
+    print("\n")
+
+    back = input("Press 'b' to go back to the Main Menu: ")
+
+    match back:
+        case 'b':
+            clear_screen()
+            
 
 # TODO 4: Create a function that stores the house rules of the game
