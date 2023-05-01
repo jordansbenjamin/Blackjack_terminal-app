568e67463d4596a517370ea08a94e0e68400095f
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-01 10:49:03 +1000

    Fixed issue with test case + added a new test case for TEST 1, result: passed

--- src/test_blackjack.py
+++ src/test_blackjack.py
@@ -5,28 +5,37 @@ from calculate import *
 
 # NOTE: Running tests works with the command(pytest -s)
 
-# CALCULATE SCORE TESTS
+### TEST 1 ###
+# CALCULATE SCORE TESTS - This test will assert if the function for calculating the score from the list of cards adds up correctly and returns the sum
 def test_calculate_score():
-    # TEST CASE 1: Test player with a hand of two cards and test the sum of their hand
+    # TEST CASE 1: Test player with a hand of two cards and test the return of the sum
     player_hand = ['♦2', '♠8']
     # Expected score
-    player_score = 10
+    expected_player_score = 10
     # Expected Result: Pass
-    assert calculate_score(player_hand) == player_score
+    assert calculate_score(player_hand) == expected_player_score
     # Result: Pass ✅
 
-    # TEST CASE 2: Test dealer with a hand of four cards and test the sum of their hand
-    dealer_hand = ['♣6', '♥7']
+    # TEST CASE 2: Test dealer with a hand of four cards and test the return of the sum
+    dealer_hand = ['♣5', '♥7', '♠3', '♦4',]
     # Expected score
-    dealer_score = 13
+    expected_dealer_score = 19
     # Expected Result: Pass
-    assert calculate_score(dealer_hand) == dealer_score
+    assert calculate_score(dealer_hand) == expected_dealer_score
     # Result: ✅
 
-    # TEST CASE 3: Test player with a hand of six cards and test the sum of their hand
+    # TEST CASE 3: Test player with a hand of six cards and test the return of the sum
     player_hand = ['♣4', '♥2', '♦4', '♠8', '♦2', '♠8']
     # Expected score
-    player_score = 28
+    expected_player_score = 28
     # Expected Result: Pass
-    assert calculate_score(player_hand) == player_score
+    assert calculate_score(player_hand) == expected_player_score
+    # Result: ✅
+
+    # TEST CASE 4: Test dealer with natural Blacjack hand (two cards) and returns the sum
+    dealer_hand = ['♥A', '♦K']
+    # Expected score
+    expected_dealer_score = 21
+    # Expected Result: Pass
+    assert calculate_score(dealer_hand) == expected_dealer_score
     # Result: ✅
