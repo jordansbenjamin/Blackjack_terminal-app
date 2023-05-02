77255c0f99c2e457a3c4a3bfed27f185855b6c07
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-02 14:49:15 +1000

    Finished tasks for scores section

--- src/scores.py
+++ src/scores.py
@@ -1,5 +1,20 @@
 ##### CSV SECTION #####
+import csv
+
+game_history = 'game_history.csv'
 
 # TODO 1: Create a function that creates a csv file when one hasn't exist
+def create_csv_file():
+    '''This function creates the CSV file to store game history and scores.'''
+    with open(game_history, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Game', 'Player Hand', 'Dealer Hand', 'Result'])
 
 # TODO 2: Create a function that counts the amount of games played and records that into the CSV
+def count_games_played():
+    try:
+        with open(game_history, 'r') as file:
+            reader = csv.reader(file)
+            return sum(1 for row in reader)
+    except FileNotFoundError:
+        return 1
