ada3bfb62122c2285ce754a5f11c14d11df76268
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-02 16:45:13 +1000

    Managed to get CSV writing and reading to work + implemented pretty table as well

--- src/scores.py
+++ src/scores.py
@@ -1,5 +1,6 @@
 ##### CSV SECTION #####
 import csv
+import os
 from prettytable import PrettyTable
 
 game_history = 'game_history.csv'
@@ -7,9 +8,10 @@ game_history = 'game_history.csv'
 # TODO 1: Create a function that creates a csv file when one hasn't exist
 def create_csv_file():
     '''This function creates the CSV file to store game history and scores.'''
-    with open(game_history, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Game', 'Player Hand', 'Dealer Hand', 'Result'])
+    if not os.path.isfile(game_history):
+        with open(game_history, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Game', 'Player Hand', 'Dealer Hand', 'Result'])
 
 # TODO 2: Create a function that counts the amount of games played and records that into the CSV
 def count_games_played():
@@ -32,10 +34,10 @@ def view_game_history():
     try:
         with open(game_history, 'r') as file:
             reader = csv.reader(file)
-            # header = next(reader)
-            # print(f"{' - '.join(header)}")
-            next(reader)
+            headers = next(reader)
+            table = PrettyTable(headers)
             for row in reader:
-                print(f"Game {row[0]} - Player Hand: {row[1]}, Dealer Hand: {row[2]}, Result: {row[3]}")
+                table.add_row(row)
+            print(table)
     except FileNotFoundError:
         print("Game history not found")
