d107d4235972a93e3215711639c0a40947867a71
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-02 11:04:55 +1000

    Refactored determine winner function for simplicity

--- src/winner.py
+++ src/winner.py
@@ -15,22 +15,24 @@ def determine_winner():
     print("\n")
     # Determine condition of winner/loser based on Natural Blackjack, which means that they have a 21 from the first cards dealt, return their outcome
     if player_score == 21 and len(player_hand) == 2:
-        return "Instant win! With a natural Blackjack 🤠"
+        result = "Instant win! With a natural Blackjack 🤠"
     elif dealer_score == 21 and len(dealer_hand) == 2:
-        return "You lose! Dealer wins with a natural Blacjack 🤯"
+        result = "You lose! Dealer wins with a natural Blacjack 🤯"
     # Determine condition of winner/loser based on normal Blackjack, which means that they have a 21 from cards drawn throughout the game, return their outcome
-    if player_score == 21:
-        return "You won with a Blackjack! 😎"
+    elif player_score == 21:
+        result = "You won with a Blackjack! 😎"
     elif dealer_score == 21:
-        return "You lose! Dealer wins with a Blackjack 😤"
+        result = "You lose! Dealer wins with a Blackjack 😤"
     # Determine all other possible winning/losing conditions, return their outcome
-    if player_score > 21:
-        return "You went over! And that's game over.. 😩"
+    elif player_score > 21:
+        result = "You went over! And that's game over.. 😩"
     elif dealer_score > 21:
-        return "Dealer went over.. Lucky you, you win! 😅"
+        result = "Dealer went over.. Lucky you, you win! 😅"
     elif player_score > dealer_score:
-        return "You're a winner! You have a higher score than the Dealer 🥳"
+        result = "You're a winner! You have a higher score than the Dealer 🥳"
     elif player_score < dealer_score:
-        return "You lost... The dealer has a higher score 🙃"
-    elif player_score == dealer_score:
-        return "It's a draw! 🥲"
+        result = "You lost... The dealer has a higher score 🙃"
+    else:
+        result = "It's a draw! 🥲"
+    
+    return result
