fdada76c778ee3fb76757597d38c67acf4ec455f
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-02 14:43:01 +1000

    Added to dos for CSV section

---
+++ src/scores.py
@@ -0,0 +1,5 @@
+##### CSV SECTION #####
+
+# TODO 1: Create a function that creates a csv file when one hasn't exist
+
+# TODO 2: Create a function that counts the amount of games played and records that into the CSV
