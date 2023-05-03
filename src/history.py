de81f3035d7f40eba4143df56422f591f9980610
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-03 17:21:48 +1000

    Added error handling to history section

--- src/history.py
+++ src/history.py
@@ -10,10 +10,16 @@ game_history = 'game_history.csv'
 def create_csv_file():
     '''This function creates the CSV file to store game history and scores.'''
 
-    if not os.path.isfile(game_history):
-        with open(game_history, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Game', 'Player Hand', 'Player Score', 'Dealer Hand', 'Dealer Score', 'Result'])
+    try:
+        if not os.path.isfile(game_history):
+            with open(game_history, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Game', 'Player Hand', 'Player Score', 'Dealer Hand', 'Dealer Score', 'Result'])
+    except FileNotFoundError:
+        print("File Not Found.")
+    except PermissionError:
+        print("Permission denied. Please make sure you have permission to create a file in the specified directory.")
+
 
 def count_games_played():
     '''This function will count the total number of games played by checking the number of rows written in the CSV file'''
@@ -29,9 +35,15 @@ def write_game_history(player_hand, player_score ,dealer_hand, dealer_score ,win
     '''This function will write and record the outcome and history of each game played to the CSV file'''
 
     game_num = count_games_played()
-    with open(game_history, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([game_num, player_hand, player_score, dealer_hand, dealer_score, winner])
+
+    try:
+        with open(game_history, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([game_num, player_hand, player_score, dealer_hand, dealer_score, winner])
+    except IOError:
+        print("An error occurred while writing to the file.")
+    except Exception as Error:
+        print(f"An error occurred while writing to the file: {str(Error)}")
 
 def view_game_history():
     '''This function will allow the user to view the game history which is recorded and written in the CSV, it also implements prettytable for a better viewing experience'''
